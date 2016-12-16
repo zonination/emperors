c9f99e2e8430ea0d46f1b1850637ec5a801de209
Zoni Nation <zonination@gmail.com>
2016-12-16 15:21:07 -0500

    Add files via upload

--- emperors.R
+++ emperors.R
@@ -1,7 +1,8 @@
-# Load Libraries, Load Data, Set working directory
+# Load Libraries, declare constants, Set working directory
 # setwd("")
 library(ggplot2)
 library(lubridate)
+dynstring = c("Julio-Claudian","Flavian","Nerva-Antonine","Severan","Gordian","Constantinian","Valentinian","Theodosian")
 
 # Load data. Note that certain cells will need formatting for
 # compatibility with ISO-8601 and R.
@@ -19,7 +20,7 @@ emperors$birth[6]<-emperors$birth[6] - 2*years(2)               # Inverting date
 emperors$name     <-factor(emperors$name,
                            levels=emperors[rev(order(emperors$index)),"name"])
 emperors$dynasty  <-factor(emperors$dynasty,
-                           levels=c("Julio-Claudian","Flavian","Nerva-Antonine","Severan","Gordian","Constantinian","Valentinian","*"))
+                           levels=dynstring)
 
 # Plot the roman emperors' Reigns and Livespans
 ggplot(emperors,aes(x=name))+
@@ -30,7 +31,22 @@ ggplot(emperors,aes(x=name))+
        subtitle="Lifespans and Reigns",
        
        x="",y="Year",
-       caption="*=Theodosian\ncreated by /u/zonination")+
+       caption="* Theodosian\ncreated by /u/zonination")+
   facet_grid(dynasty~., scales="free_y",space="free_y")+
   theme_bw()
 ggsave("Lifespan.png",width=24,height=14,units="in",dpi=100)
+
+# Temporarily reverse levels for dynasty names
+emperors$dynasty  <-factor(emperors$dynasty,
+                           levels=rev(dynstring))
+
+# Plot their deaths
+ggplot(emperors,aes(x=dynasty))+
+  geom_bar(stat="count",aes(fill=cause),color="black")+
+  coord_flip()+
+  theme_bw()
+
+ggplot(emperors,aes(x=dynasty))+
+  geom_bar(stat="count",aes(fill=killer),color="black")+
+  coord_flip()+
+  theme_bw()
